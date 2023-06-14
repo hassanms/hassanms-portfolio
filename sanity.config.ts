2039c380ff16c23d960408ec8cad4f95c77768c8
Hassan MS <1engr.hassan@gmail.com>
2023-06-14 20:46:16 +0500

    First Sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
     projectId: 'luj5ez6g',
@@ -8,6 +9,7 @@ const config = defineConfig({
     apiVersion: '2023-06-13',
     basePath: "/admin",
     plugins: [deskTool()],
+    schemas: { types: schemas }
 });
 
 export default config;
